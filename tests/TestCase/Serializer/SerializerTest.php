e44b1c0d805580668105d960aa7b8754371d76f1
Andrew Shelyakov <andrew.shelyakov@eggheads.solutions>
2022-07-20 23:33:58 +0300

    Обеспечил покрытие новых методов тестами.

--- tests/TestCase/Serializer/SerializerTest.php
+++ tests/TestCase/Serializer/SerializerTest.php
@@ -9,6 +9,7 @@ use Eggheads\CakephpCommon\Lib\Arrays;
 use Eggheads\CakephpCommon\TestSuite\AppTestCase;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
+use Throwable;
 
 class SerializerTest extends AppTestCase
 {
@@ -192,5 +193,122 @@ class SerializerTest extends AppTestCase
             ['field_int' => 123, 'objects' => []],
             $result->toArray(true, [AbstractObjectNormalizer::SKIP_NULL_VALUES => true])
         );
+    }
+
+    /**
+     * Проверяем создание массива объектов из массива массивов.
+     *
+     * @return void
+     * @throws Throwable
+     * @group simple
+     */
+    public function testCreateManyFromArray(): void
+    {
+        $fromData = [
+            ['fieldInt' => 3],
+            ['fieldInt' => 7, 'fieldObject' => [
+                'fieldInt' => '9',
+                'fieldString' => 12345,
+                'objects' => [],
+            ]],
+        ];
+
+        $expected = [
+            ['fieldInt' => 3, 'fieldString' => null, 'fieldObject' => null, 'objects' => []],
+            ['fieldInt' => 7, 'fieldString' => null, 'fieldObject' => [
+                'fieldInt' => '9',
+                'fieldString' => '12345',
+                'fieldObject' => null,
+                'objects' => [],
+            ], 'objects' => []],
+        ];
+
+        $result = RequestTest::createManyFromArray($fromData);
+        $actualAsArray = array_map(fn(RequestTest $item) => $item->toArray(), $result);
+
+        self::assertEquals($expected, $actualAsArray);
+    }
+
+    /**
+     * Проверяем создание массива объектов из json.
+     *
+     * @return void
+     * @throws Throwable
+     * @group simple
+     */
+    public function testCreateManyFromJson(): void
+    {
+        $fromData = Arrays::encode([
+            ['fieldInt' => 3],
+            ['fieldInt' => 7, 'fieldObject' => [
+                'fieldInt' => '9',
+                'fieldString' => 'exampleString',
+                'fieldObject' => null,
+                'objects' => [],
+            ]],
+        ]);
+
+        $expected = [
+            ['fieldInt' => 3, 'fieldString' => null, 'fieldObject' => null, 'objects' => []],
+            ['fieldInt' => 7, 'fieldString' => null, 'fieldObject' => [
+                'fieldInt' => '9',
+                'fieldString' => 'exampleString',
+                'fieldObject' => null,
+                'objects' => [],
+            ], 'objects' => []],
+        ];
+
+        $result = RequestTest::createManyFromJson($fromData);
+        $actualAsArray = array_map(fn(RequestTest $item) => $item->toArray(), $result);
+
+        self::assertEquals($expected, $actualAsArray);
+    }
+
+    /**
+     * Проверяем валидацию при создании массива объектов с глубоким уровнем вложенности.
+     *
+     * @return void
+     * @throws Throwable
+     * @group simple
+     */
+    public function testCreateManyDeepValidation(): void
+    {
+        $this->expectExceptionMessage('Не указан fieldInt, Не указан fieldInt');
+
+        $data = [
+            [
+                'fieldInt' => 8,
+                'fieldObject' => [
+                    'fieldInt' => 9,
+                    'fieldObject' => ['fieldInt' => 19],
+                    'objects' => [
+                        ['fieldInt' => 10],
+                        [
+                            'fieldInt' => 11,
+                            'objects' => [
+                                ['fieldInt' => 12],
+                                [
+                                    'fieldInt' => 13,
+                                    'fieldObject' => [
+                                        'fieldInt' => 14,
+                                        'objects' => [
+                                            ['fieldInt' => 15],
+                                            ['fieldString' => 'exampleString4'],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                'fieldInt' => 8,
+                'fieldObject' => [
+                ],
+            ],
+        ];
+
+        RequestTest::createManyFromArray($data);
     }
 }
