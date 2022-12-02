b3d5678235b10b1628d45ce784ad9ee3a4055f7c
mrAlheor <mrAlheor@gmail.com>
2022-12-02 16:55:05 +0300

    [IT-915] bugs and deprecation fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,11 +8,12 @@ require $rootFolder . '/config/bootstrap_test.php';
 Cake\Core\Plugin::getCollection()->add(new \Migrations\Plugin());
 Cake\Core\Plugin::getCollection()->add(new \CakephpFixtureFactories\Plugin());
 
-use Migrations\Migrations;
 use Migrations\TestSuite\Migrator;
 
 $migrator = new Migrator();
 $migrator->run();
 
 //Выполнение миграций для PostgreSql
-(new Migrations(['connection' => 'test_postgres', 'source' => './Migrations/Postgres']))->migrate();
+(new Migrator())->run(
+    ['connection' => 'test_postgres', 'source' => './Migrations/Postgres']
+);
